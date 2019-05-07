cd7fcc2fb2fc673c4608506359cf98f14848f4b3
Chris <chris.daf@gmail.com>
2019-05-07 14:55:33 +0100

    Challenge 5: add top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it "displays a balance of 0 on a new card" do
     expect(subject.balance).to eq(0)
   end
+
+  it "adds a value to the card balance" do
+    subject.top_up(4)
+    expect(subject.balance).to eq(4)
+  end
+
 end
